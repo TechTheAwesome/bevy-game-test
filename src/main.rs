315c0c2eaa6d2b63fb767be6138931eafea78838
TechTheAwesome <techtheawesome01@gmail.com>
2022-06-17 18:22:39 +0900

    ADD debug feature and inspector_egui dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::{Inspectable, WorldInspectorPlugin, RegisterInspectable};
+#[cfg(feature = "debug")]
+#[derive(Inspectable, Component)]
+struct InspectableType;
+#[cfg(feature = "debug")]
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+struct ReflectedType;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    #[cfg(feature = "debug")]
+    app.add_plugin(WorldInspectorPlugin::new())
+        .register_inspectable::<InspectableType>()
+        .register_type::<ReflectedType>();
+
+    app.add_plugins(DefaultPlugins);
+    app.add_startup_system(setup_cameras);
+
+    app.run();
+}
+
+fn setup_cameras(mut commands: Commands) {
+    // Cameras
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
 }
